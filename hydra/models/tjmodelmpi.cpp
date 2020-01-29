e7db3acf7a15e78e1a3b6c1d7341671962dc88ae
Henry Shackleton <hshackle@mit.edu>
2020-01-29 14:12:46 -0500

    Added exchange terms and fixed convergence issues

--- hydra/models/tjmodelmpi.cpp
+++ hydra/models/tjmodelmpi.cpp
@@ -49,6 +49,204 @@ namespace hydra { namespace models {
 
       verbose = true;
 
+      Zeros(out_vec.vector_local());
+
+      // Apply szsz terms
+      
+      int szsz_idx = 0;
+      for (auto pair : szszs_)
+      {
+        const int s1 = std::min(pair.first, pair.second);
+        const int s2 = std::max(pair.first, pair.second);
+        const double jz = szsz_amplitudes_[szsz_idx]*0.25;
+        if (std::abs(jz) > 1e-14)
+          {
+            uint64 upspin_idx = 0;
+            for (const state_t& upspins : my_upspins_)
+            {
+            uint64 upspin_offset = my_upspins_offset_[upspins];
+            uint64 downspin_offset=0;
+            for (state_t holes: hs_holes_in_ups_)
+              {
+                state_t downspins = up_hole_to_down(upspins, holes);
+                uint64 idx = upspin_offset + downspin_offset;
+                auto coeff =
+            jz*(((double)gbit(upspins, s1) - (double)gbit(downspins, s1)) *
+          ((double)gbit(upspins, s2) - (double)gbit(downspins, s2)));
+                out_vec(idx) += coeff * in_vec(idx);
+                ++downspin_offset;
+              }
+            ++upspin_idx;
+            }
+          }
+        ++szsz_idx;
+      }
+      
+
+
+      // Spin exchange terms
+      
+      int exchange_idx=0;
+      for (auto pair: exchanges_)
+      {
+        const int s1 = std::min(pair.first, pair.second);
+        const int s2 = std::max(pair.first, pair.second);
+        const coeff_t jx = exchange_amplitudes_[exchange_idx]*0.5;
+        const state_t flipmask = ((state_t)1 << s1) | ((state_t)1 << s2);
+
+        // Find out how many states is sent to each process
+        std::vector<int> n_states_i_send(mpi_size_, 0);
+        if (std::abs(jx) > 1e-14)
+        {
+          // Flip states and check out how much needs to be communicated
+          for (const state_t& upspins : my_upspins_)
+          for (state_t holes : hs_holes_in_ups_)
+            {
+              state_t downspins = up_hole_to_down(upspins, holes);
+
+              if ((popcnt(upspins & flipmask) == 1) && 
+                  (popcnt(downspins & flipmask) == 1))
+                  {
+                  state_t flipped_upspins = upspins ^ flipmask;
+                  int target = mpi_rank_of_spins(flipped_upspins);
+                  ++n_states_i_send[target];
+                  }
+            }
+
+            // Exchange information on who sends how much to whom
+            std::vector<int> n_states_i_recv(mpi_size_, 0);
+            MPI_Alltoall(n_states_i_send.data(), 1, MPI_INT, n_states_i_recv.data(),
+              1, MPI_INT, MPI_COMM_WORLD);
+
+            // Sum up states sent/recvd and eventually resize buffers
+            uint64 sum_n_states_i_send = std::accumulate(n_states_i_send.begin(), 
+              n_states_i_send.end(), (uint64)0);
+            uint64 sum_n_states_i_recv = std::accumulate(n_states_i_recv.begin(),
+              n_states_i_recv.end(), (uint64)0);
+
+            if (sum_n_states_i_send > send_buffer_.size())
+              send_buffer_.resize(sum_n_states_i_send);
+            if (sum_n_states_i_recv > recv_buffer_.size())
+              recv_buffer_.resize(sum_n_states_i_recv);
+
+            // Compute offsets of send/recieve states
+            std::vector<int> n_states_i_send_offsets(mpi_size_, 0);
+            std::vector<int> n_states_i_recv_offsets(mpi_size_, 0);
+            for (int m = 0; m < mpi_size_; ++m) 
+              for (int n = 0; n < m; ++n)
+              {
+                n_states_i_send_offsets[m] += n_states_i_send[n];
+                n_states_i_recv_offsets[m] += n_states_i_recv[n];
+              }
+            
+            // Flip states and check how much needs to be communicated
+            std::vector<int> n_states_prepared(mpi_size_, 0);
+            for (const state_t& upspins : my_upspins_)
+            {
+              uint64 upspin_offset = my_upspins_offset_[upspins];
+              uint64 downspin_offset=0;
+              for (state_t holes: hs_holes_in_ups_)
+                {
+                  state_t downspins = up_hole_to_down(upspins, holes);
+
+                  if ((popcnt(upspins & flipmask) == 1) &&
+                (popcnt(downspins & flipmask) == 1))
+              {
+                uint64 idx = upspin_offset + downspin_offset;
+                state_t flipped_upspins = upspins ^ flipmask;
+                int target = mpi_rank_of_spins(flipped_upspins);
+                int send_idx = n_states_i_send_offsets[target] +
+                  n_states_prepared[target];
+
+                send_buffer_[send_idx] = in_vec.vector_local()(idx);
+                ++n_states_prepared[target];
+              }
+                  ++downspin_offset;
+              }
+            }
+
+                 //LilaPrint(in_vec.vector_local());
+                 //for (int i=0; i<sum_n_states_i_send; ++i)
+                 //	printf("send_buf[%d] = %f\n", i, send_buffer_[i]);
+
+
+                // Check, whether correct number of states has been prepared
+                for(int m = 0; m < mpi_size_; ++m)
+            assert(n_states_prepared[m] == n_states_i_send[m]);
+
+                // Alltoall call
+                lila::MPI_Alltoallv<coeff_t>
+            (send_buffer_.data(), n_states_i_send.data(),
+             n_states_i_send_offsets.data(),
+             recv_buffer_.data(), n_states_i_recv.data(),
+             n_states_i_recv_offsets.data(),
+             MPI_COMM_WORLD);
+
+                // for (int i=0; i<sum_n_states_i_recv; ++i)
+                // 	printf("recv_buf[%d] = %f\n", i, recv_buffer_[i]);
+
+                // Get the original upspin configuration and its source proc
+                std::vector<std::vector<state_t>> upspins_i_get_from_proc(mpi_size_);
+                for (const state_t& upspins : my_upspins_)
+            {
+              state_t flipped_upspins = upspins ^ flipmask;
+              int source = mpi_rank_of_spins(flipped_upspins);
+              upspins_i_get_from_proc[source].push_back(upspins);
+            }
+
+                // for (int i=0; i<mpi_size_; ++i)
+                // 	for (auto upspins : upspins_i_get_from_proc[i])
+                // 	  printf("upspins_i_get: %d\n", upspins);
+
+
+                // Sort the upspins and add coefficients to outvec
+                //LilaPrint(out_vec.vector_local());
+
+                uint64 recv_idx=0;
+                for (int m=0; m<mpi_size_; ++m)
+            {
+
+              // Sort according to order of flipped upspins
+              std::sort(upspins_i_get_from_proc[m].begin(),
+                  upspins_i_get_from_proc[m].end(),
+                  [&flipmask](state_t const& a, state_t const& b)
+                  {
+                    state_t flipped_a = a ^ flipmask;
+                    state_t flipped_b = b ^ flipmask;
+                    return flipped_a < flipped_b;
+                  });
+
+              for (const state_t& upspins : upspins_i_get_from_proc[m])
+                {
+                  uint64 upspin_offset = my_upspins_offset_[upspins];
+
+                  uint64 downspin_offset=0;
+                  for (state_t holes : hs_holes_in_ups_)
+              {
+                    state_t downspins = up_hole_to_down(upspins, holes);
+                if ((popcnt(upspins & flipmask) == 1) &&
+                    (popcnt(downspins & flipmask) == 1))
+                  {
+                    uint64 target_idx = upspin_offset + downspin_offset;
+                     //printf("upspins_offset: %d, target_idx: %d, recv_idx: %d, coeff: %f\n",
+                     	//     upspin_offset, target_idx, recv_idx, jx * recv_buffer_[recv_idx] );
+
+                    out_vec.vector_local()(target_idx) += jx *
+                recv_buffer_[recv_idx];
+                // LilaPrint(out_vec.vector_local());
+                    ++recv_idx;
+                  }
+                ++downspin_offset;
+              }
+              }
+            }
+                 //LilaPrint(out_vec.vector_local());
+
+              }
+            ++exchange_idx;
+          }
+
+
       // Apply hoppings on downspins
       double t1 = MPI_Wtime();
       int hopping_idx=0;
