15d932ebe5946a39b1ff47c4cbd4a6c2beb1e516
Henry <hshackle@mit.edu>
2020-06-12 15:42:56 -0400

    Added exchange terms in hubbardfulled and tests

--- hydra/models/hubbardmodel.cpp
+++ hydra/models/hubbardmodel.cpp
@@ -115,26 +115,31 @@ namespace hydra { namespace models {
 	  const int s2 = std::max(pair.first, pair.second);
 	  const coeff_t t = hopping_amplitudes_[hopping_idx];
 	  const uint32 flipmask = ((uint32)1 << s1) | ((uint32)1 << s2);
+    const uint32 secondmask = (uint32)1 << s2;
 
 	  if (std::abs(t) > 1e-14)
 	    {
 	      for (int idx : range<>(indexing.size()))
-		{
+		  {
 		  auto state = indexing.state(idx);
 		  const uint32& upspins = state.upspins;
 		  const uint32& downspins = state.downspins;
-		
+
 		  // upspins hopping
 		  if (((upspins & flipmask) != 0) && 
 		      ((upspins & flipmask) != flipmask))
 		    {
 		      const double fermi = 
-			popcnt(gbits(upspins, s2-s1-1, s1+1)) % 2==0 ? 1. : -1.;
+			popcnt(gbits(upspins ^ downspins, s2-s1, s1)) % 2==0 ? 1. : -1.;
 		      // printf("fermi %f\n", fermi);
 		      auto new_state = state;
 		      new_state.upspins = upspins ^ flipmask;
 		      int new_idx = indexing.index(new_state);
-		      hamilton(new_idx, idx) -= fermi*t;
+          if (upspins & secondmask) {
+		        hamilton(new_idx, idx) += fermi*t;
+        } else { 
+            hamilton(new_idx, idx) -= lila::conj(t) * fermi;
+        }
 		    }
 
 		  // downspins hopping
@@ -142,20 +147,278 @@ namespace hydra { namespace models {
 		      ((downspins & flipmask) != flipmask))
 		    {
 		      const double fermi = 
-			popcnt(gbits(downspins, s2-s1-1, s1+1)) % 2==0 ? 1.:-1.;
+			popcnt(gbits(upspins ^ (downspins ^ secondmask), s2-s1, s1+1)) % 2==0 ? 1.:-1.;
 		      // printf("fermi %f\n", fermi);
 		      auto new_state = state;
 		      new_state.downspins = downspins ^ flipmask;
 		      int new_idx = indexing.index(new_state);
-		      hamilton(new_idx, idx) -= fermi*t;
+          if (downspins & secondmask) {
+		        hamilton(new_idx, idx) += fermi*t;
+        } else {
+            hamilton(new_idx, idx) -= lila::conj(t) * fermi;
+        }
 		    }
 		}
 	    }
 	  ++hopping_idx;
 	}  // loop over hoppings
 
+      // SzSz terms
+      int szsz_idx = 0;
+      for (auto pair : szszs_)
+      {
+        int s1 = pair.first;
+        int s2 = pair.second;
+        double jz = szsz_amplitudes_[szsz_idx]*0.25;
+
+        if (std::abs(jz) > 1e-14)
+        {
+          for (int idx : range<>(indexing.size()))
+          {
+            auto state = indexing.state(idx);
+            const uint32& upspins = state.upspins;
+            const uint32& downspins = state.downspins;
+
+            bool up1 = gbit(upspins, s1);
+            bool up2 = gbit(upspins, s2);
+            bool dn1 = gbit(downspins, s1);
+            bool dn2 = gbit(downspins, s2);
+//      			auto coeff = jz*(((double)gbit(upspins, s1) - (double)gbit(downspins, s1)) *
+//			    ((double)gbit(upspins, s2) - (double)gbit(downspins, s2)));
+
+            if (!(up1 && dn1) && !(dn2 && up2))
+            {
+              if ((up1 && up2) || (dn1 && dn2))
+                hamilton(idx, idx) += jz;
+              else if ((up1 && dn2) || (dn1 && up2))
+                hamilton(idx, idx) += -jz;
+          }
+          }
+        }
+        ++szsz_idx;
+      }
+
+        // Exchange terms
+        int exchange_idx=0;
+        for (auto pair: exchanges_)
+        {
+          int s1 = std::min(pair.first, pair.second);
+          int s2 = std::max(pair.first, pair.second);
+          coeff_t jx = exchange_amplitudes_[exchange_idx]*0.5;
+          const uint32 flipmask = ((uint32)1 << s1) | ((uint32)1 << s2);
+          if (std::abs(jx) > 1e-14)
+            {
+              for (int idx : range<>(indexing.size()))
+          {
+            auto state = indexing.state(idx);
+            const uint32& upspins = state.upspins;
+            const uint32& downspins = state.downspins;
+          
+      		  if ((popcnt(upspins & flipmask) == 1) &&
+      			(popcnt(downspins & flipmask) == 1)&& popcnt((downspins & flipmask) & (upspins & flipmask)) == 0)
+//if ((up1 && dn2 && !up2 && !dn1) || (dn1 && up2 && !dn2 && !up1))
+              {
+                auto new_state = state;
+                new_state.upspins = upspins ^ flipmask;
+                new_state.downspins = downspins ^ flipmask;
+
+                if (popcnt(new_state.upspins) != popcnt(state.upspins))
+            {
+              using namespace hilbertspaces;
+              std::cout << PrintSpinhalf(32, state.upspins)
+                  << std::endl
+                  << s1 << " " << s2
+                  << std::endl
+                  << popcnt(state.upspins) << std::endl
+                  << PrintSpinhalf(32, new_state.upspins)
+                  << std::endl
+                  << popcnt(new_state.upspins) << std::endl
+                  << std::endl;
+            }
+                assert(popcnt(new_state.upspins) == popcnt(state.upspins));
+                assert(popcnt(new_state.downspins) == popcnt(state.downspins));
+
+		            int flipped_idx = indexing.index(new_state);
+                hamilton(flipped_idx, idx) += jx;
+              }
+
+          }  // loop over spin configurations
+            }  // if (std::abs(jx) > 1e-14)
+          ++exchange_idx;
+        }  // for (auto pair: exchanges_)
+
+
+
+
+
       return hamilton;
     }
+
+    template <class coeff_t>
+    lila::Matrix<double> HubbardModel<coeff_t>::szMatrix(int siteIndex) const
+    {
+      using hilbertspaces::Spinhalf;
+      using hilbertspaces::Hubbard;
+      using indexing::IndexTable;
+      using indexing::IndexHubbard;
+      using utils::range;
+      using utils::gbit;
+      using utils::gbits;
+      using utils::popcnt;
+
+      assert(0 <= siteIndex && siteIndex < n_sites_);
+	
+      Hubbard<uint32> hs(n_sites_, qn_);
+      IndexHubbard<IndexTable<Spinhalf<uint32>, uint32>> indexing(hs);
+      int dim = indexing.size();
+      lila::Matrix<double> sz(dim, dim);
+      lila::Zeros(sz);
+
+      // Assemble sz matrix
+      
+        for (int idx : range<>(indexing.size()))
+        {
+          auto state = indexing.state(idx);
+          const uint32& upspins = state.upspins;
+          const uint32& downspins = state.downspins;
+          bool ups = gbit(upspins, siteIndex);
+          bool dns = gbit(downspins, siteIndex);
+
+          sz(idx, idx) = ups - dns;
+        }
+      return sz;
+      }  
+
+
+    template <class coeff_t>
+    lila::Matrix<double> HubbardModel<coeff_t>::sPlusMatrix(int siteIndex) const
+    {
+      using hilbertspaces::Spinhalf;
+      using hilbertspaces::Hubbard;
+      using indexing::IndexTable;
+      using indexing::IndexHubbard;
+      using utils::range;
+      using utils::gbit;
+      using utils::gbits;
+      using utils::popcnt;
+
+      assert(0 <= siteIndex && siteIndex < n_sites_);
+	
+      Hubbard<uint32> hs(n_sites_, qn_);
+      IndexHubbard<IndexTable<Spinhalf<uint32>, uint32>> indexing(hs);
+
+      if (qn_.n_upspins < n_sites_ && qn_.n_downspins > 0) {
+
+      // Initialize Hilbert space for target space
+      auto target_qn = qn_;
+      ++target_qn.n_upspins;
+      --target_qn.n_downspins;
+      Hubbard<uint32> target_hs(n_sites_, target_qn);
+      IndexHubbard<IndexTable<Spinhalf<uint32>, uint32>> target_indexing(target_hs);
+
+      int base_dim = indexing.size();
+      int target_dim= target_indexing.size();
+      lila::Matrix<double> sPlus;
+      sPlus.resize(target_dim, base_dim);
+      lila::Zeros(sPlus);
+
+      // Assemble sPlus matrix
+    
+      uint32 flipmask = ((uint32)1 << siteIndex);
+      
+        for (int idx : range<>(indexing.size()))
+        {
+          auto state = indexing.state(idx);
+          const uint32& upspins = state.upspins;
+          const uint32& downspins = state.downspins;
+          bool ups = gbit(upspins, siteIndex);
+          bool dns = gbit(downspins, siteIndex);
+
+          if (dns && !ups) {
+            auto flipped_state = state;
+            flipped_state.upspins = state.upspins ^ flipmask;
+            flipped_state.downspins = state.downspins ^ flipmask;
+            int target_idx = target_indexing.index(flipped_state);
+
+            sPlus(target_idx, idx) = 1;
+          }
+        }
+      return sPlus;
+      } else {
+      lila::Matrix<double> sPlus(1, 1);
+      lila::Zeros(sPlus);
+      return sPlus;
+      }
+      }  
+
+
+    template <class coeff_t>
+    lila::Matrix<double> HubbardModel<coeff_t>::sMinusMatrix(int siteIndex) const
+    {
+      using hilbertspaces::Spinhalf;
+      using hilbertspaces::Hubbard;
+      using indexing::IndexTable;
+      using indexing::IndexHubbard;
+      using utils::range;
+      using utils::gbit;
+      using utils::gbits;
+      using utils::popcnt;
+
+      assert(0 <= siteIndex && siteIndex < n_sites_);
+	
+      Hubbard<uint32> hs(n_sites_, qn_);
+      IndexHubbard<IndexTable<Spinhalf<uint32>, uint32>> indexing(hs);
+
+      if (qn_.n_downspins < n_sites_ && qn_.n_upspins > 0) {
+
+      // Initialize Hilbert space for target space
+      auto target_qn = qn_;
+      --target_qn.n_upspins;
+      ++target_qn.n_downspins;
+      Hubbard<uint32> target_hs(n_sites_, target_qn);
+      IndexHubbard<IndexTable<Spinhalf<uint32>, uint32>> target_indexing(target_hs);
+
+      int base_dim = indexing.size();
+      int target_dim= target_indexing.size();
+      lila::Matrix<double> sMinus;
+      sMinus.resize(target_dim, base_dim);
+      lila::Zeros(sMinus);
+
+      // Assemble sMinus matrix
+    
+      uint32 flipmask = ((uint32)1 << siteIndex);
+      
+        for (int idx : range<>(indexing.size()))
+        {
+          auto state = indexing.state(idx);
+          const uint32& upspins = state.upspins;
+          const uint32& downspins = state.downspins;
+          bool ups = gbit(upspins, siteIndex);
+          bool dns = gbit(downspins, siteIndex);
+
+          if (!dns && ups) {
+
+            auto flipped_state = state;
+            flipped_state.upspins = state.upspins ^ flipmask;
+            flipped_state.downspins = state.downspins ^ flipmask;
+            int target_idx = target_indexing.index(flipped_state);
+
+            sMinus(target_idx, idx) = 1;
+          }
+        }
+      return sMinus;
+      } else {
+      lila::Matrix<double> sMinus(1, 1);
+      lila::Zeros(sMinus);
+      return sMinus;
+      }
+      }  
+
+
+
+
+
+
 
     template <class coeff_t>
     void HubbardModel<coeff_t>::apply_hamiltonian
