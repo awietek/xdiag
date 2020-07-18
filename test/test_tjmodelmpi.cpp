50489085b750f02513d70efe3029d7a9f864008c
Henry Shackleton <hshackle@mit.edu>
2020-07-18 12:33:46 -0400

    added test for sz operator

--- test/test_tjmodelmpi.cpp
+++ test/test_tjmodelmpi.cpp
@@ -5,6 +5,8 @@
 
 #include "testcases_tjmodel.h"
 
+#include <stdio.h>
+
 using namespace hydra::all;
 using namespace lila;
 
@@ -65,6 +67,74 @@ void test_tjmodelmpi(BondList bondlist,  Couplings couplings)
 	
       }
 
+}
+
+// Checks whether tjmodelmpi's application of sz is the same as in fulled
+template <class coeff_t>
+void test_tjmodelmpi_sz(BondList bondlist,  Couplings couplings)
+{
+  int n_sites = bondlist.n_sites();
+  int mpi_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+
+  
+  for (int nup=1; nup<=n_sites; ++nup)
+    for (int ndn=1; ndn<=n_sites-nup; ++ndn)
+      {
+        std::cout << "nup=" << nup << " ndn=" << ndn << std::endl;
+	hubbard_qn qn = {nup, ndn};
+	auto model = TJModel<coeff_t>(bondlist, couplings, qn);
+	auto HM = model.matrix();
+	REQUIRE(lila::close(HM, lila::Herm(HM)));
+	auto fullRes = lila::EigenSym(HM);
+  auto fullEDEigenvectors = fullRes.eigenvectors;
+  auto full_eigs = fullRes.eigenvalues;
+
+  // Check to make sure there is a unique ground state
+  if (std::abs(full_eigs(0) - full_eigs(1)) > 1e-2) { 
+    auto fullGroundstate = fullEDEigenvectors.col(0);
+    auto sz = model.szMatrix(0);
+    auto szGS = Dot(fullGroundstate, Mult(sz, fullGroundstate));
+
+    int random_seed = 42 + 1234567*mpi_rank;
+    normal_dist_t<coeff_t> dist(0., 1.);
+    normal_gen_t<coeff_t> gen(dist, random_seed);
+
+
+    
+    auto H = TJModelMPI<coeff_t>(bondlist, couplings, qn);
+    auto multiply_H = 
+      [&H](const VectorMPI<coeff_t>& v, VectorMPI<coeff_t>& w) 
+      { H.apply_hamiltonian(v, w); };
+
+    int lobpcgbands = 1;
+    std::vector<VectorMPI<coeff_t>> vs;
+    for (int i=0; i<lobpcgbands; ++i)
+    {
+      VectorMPI<coeff_t> v(H.local_dim());
+      Random(v, gen);
+      vs.push_back(v);
+    }
+
+      // Run LOBPCG
+      auto res = Lobpcg(multiply_H, vs, 1e-12, 10000);
+      auto eigs = res.eigenvalues;
+      auto groundstate = res.eigenvectors[0];
+      VectorMPI<coeff_t> outstate(H.local_dim());
+  H.apply_hamiltonian(groundstate, outstate);
+    coeff_t norm = Dot(groundstate, groundstate);
+    groundstate /= sqrt(norm);
+    REQUIRE(std::abs(full_eigs(0) - res.eigenvalues(0)) < 1e-8);
+
+    // Apply sz
+     
+    VectorMPI<coeff_t> groundstateAfter;
+    H.apply_sz(groundstate, groundstateAfter, 0);
+    auto szGSLanczos = Dot(groundstateAfter, groundstate);
+    
+    REQUIRE(std::abs(szGS - szGSLanczos) < 1e-4);
+	  }	
+}
 }
 
 
@@ -210,4 +280,15 @@ TEST_CASE( "TJModelMPI", "[TJModel]" ) {
       std::tie(bondlist, couplings) = freefermion_alltoall_complex(n_sites);
       test_tjmodelmpi<complex>(bondlist, couplings);
     }
+
+  // Tests accuracy of sz operator by calulating the expectation
+  // value of sz in the ground state of a certain model.
+  // The specific model is arbitrary, as the only purpose of
+  // the model is to pick out a unique state for both the fulled and MPI
+  printf("TJModelMPI: SZ application test");
+  test_tjmodelmpi_sz<double>(bondlist, couplings);
+  std::tie(bondlist, couplings) = tj_alltoall(6);
+  test_tjmodelmpi_sz<double>(bondlist, couplings);
+  std::tie(bondlist, couplings) = tj_alltoall_complex(6);
+  test_tjmodelmpi_sz<complex>(bondlist, couplings);
 }
