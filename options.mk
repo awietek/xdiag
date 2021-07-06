d6cd8b1b4d9a913dc6aa31e4584045d36b7e70a9
alexwie <awietek@flatironinstitute.org>
2021-07-06 19:28:45 -0400

    Got parallel Lanczos to work, first SpinhalfMPI Ising terms work

--- options.mk
+++ options.mk
@@ -1,6 +1,7 @@
 ifeq ($(arch), flatiron_linux)
 cc         = clang++
-ccopt      = -O3 -mavx -DLILA_USE_MKL
+mpicc      = mpicxx
+ccopt      = -g -mavx -DLILA_USE_MKL
 ccarch     = -std=c++17 -Wall -pedantic -m64 -Wno-return-type-c-linkage
 liladir    = /mnt/home/awietek/Research/Software/lila
 limedir    = /mnt/home/awietek/Research/Software/lime
