44eb297e979f15d43c3b7f8e10b6b15f2a6f965d
alexwie <awietek@flatironinstitute.org>
2021-08-26 17:26:49 -0400

    Removed some old code

--- hydra/old/applications/hubbardthermo/hubbardthermo.cmd
+++
@@ -1,7 +0,0 @@
-Opt std::string outfile "" o outfile outfile "name of outfile"
-Opt std::string latticefile "" l latticefile latticefile "name of latticefile"
-Opt std::string couplingfile "" c couplingfile couplingfile "name of couplingfile (default: latticefile)"
-Opt std::string ensemble "canonical" e ensemble ensemble "ensemble to use, either canonical or grandcanonical (default: canonical)" 
-Opt std::string temperaturefile "" t temperaturefile temperaturefile "name of temperaturefile"
-Opt int np -1 p np np "number of particles for canonical ensemble (optional, default half filling)"
-Opt double mu 0. m mu mu "chemical potential for grandcanonical ensemble (optional, default 0.)"
