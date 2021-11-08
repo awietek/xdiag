9d3f4af3249f5435a3c827ff4e3e0d4021d3d388
alexwie <awietek@flatironinstitute.org>
2021-11-08 17:22:43 -0500

    Added complex exchange for spinhalf

--- test/blocks/spinhalf/testcases_spinhalf.h
+++ test/blocks/spinhalf/testcases_spinhalf.h
@@ -12,4 +12,7 @@ HBchain_fullspectrum_nup(int n_sites, int nup);
 
 std::tuple<BondList, Couplings> HB_alltoall(int n_sites);
 
+std::tuple<BondList, Couplings, double> triangular_12_complex(int nup,
+                                                              double eta);
+
 } // namespace hydra::testcases::spinhalf
