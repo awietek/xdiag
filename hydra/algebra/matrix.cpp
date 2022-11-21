02f8ad0ef9476c1df7713addac05c48a1d7ed30d
Alex Wietek <awietek@pks.mpg.de>
2022-11-21 15:37:12 +0100

    Fixed issue creating matrices

--- hydra/algebra/matrix.cpp
+++ hydra/algebra/matrix.cpp
@@ -8,23 +8,25 @@ namespace hydra {
 
 arma::Mat<double> matrix_real(BondList const &bonds, Block const &block_in,
                               Block const &block_out) {
-  return std::visit(variant::overloaded{
-      [&](BondList const &bonds, Spinhalf const &block_in,
-          Spinhalf const &block_out) -> arma::Mat<double> {
-        return matrix_real(bonds, block_in, block_out);
-      },
-      [&](BondList const &bonds, tJ const &block_in,
-          tJ const &block_out) -> arma::Mat<double> {
-        return matrix_real(bonds, block_in, block_out);
-      },
-      [&](BondList const &bonds, Electron const &block_in,
-          Electron const &block_out) -> arma::Mat<double> {
-        return matrix_real(bonds, block_in, block_out);
-      },
-      [&](auto const &block_in, auto const &block_out) -> arma::Mat<double> {
-        Log.err("Error in matrix_real: Invalid blocks (combination)!");
-        return arma::mat();
-      }}, block_in, block_out);
+  return std::visit(
+      variant::overloaded{
+          [&](Spinhalf const &block_in,
+              Spinhalf const &block_out) -> arma::Mat<double> {
+            return matrix_real(bonds, block_in, block_out);
+          },
+          [&](tJ const &block_in, tJ const &block_out) -> arma::Mat<double> {
+            return matrix_real(bonds, block_in, block_out);
+          },
+          [&](Electron const &block_in,
+              Electron const &block_out) -> arma::Mat<double> {
+            return matrix_real(bonds, block_in, block_out);
+          },
+          [&](auto const &block_in,
+              auto const &block_out) -> arma::Mat<double> {
+            Log.err("Error in matrix_real: Invalid blocks (combination)!");
+            return arma::mat();
+          }},
+      block_in, block_out);
 }
 arma::Mat<double> matrix_real(Bond const &bond, Block const &block_in,
                               Block const &block_out) {
@@ -43,23 +45,24 @@ arma::Mat<double> matrix_real(Bond const &bond, Block const &block) {
 
 arma::Mat<complex> matrix_cplx(BondList const &bonds, Block const &block_in,
                                Block const &block_out) {
-  return std::visit(variant::overloaded{
-      [&](BondList const &bonds, Spinhalf const &block_in,
-          Spinhalf const &block_out) -> arma::Mat<complex> {
-        return matrix_cplx(bonds, block_in, block_out);
-      },
-      [&](BondList const &bonds, tJ const &block_in,
-          tJ const &block_out) -> arma::Mat<complex> {
-        return matrix_cplx(bonds, block_in, block_out);
-      },
-      [&](BondList const &bonds, Electron const &block_in,
-          Electron const &block_out) -> arma::Mat<complex> {
-        return matrix_cplx(bonds, block_in, block_out);
-      },
-      [&](auto const &block_in, auto const &block_out) {
-        Log.err("Error in matrix_real: Invalid blocks (combination)!");
-        return arma::cx_mat();
-      }}, block_in, block_out);
+  return std::visit(
+      variant::overloaded{
+          [&](Spinhalf const &block_in,
+              Spinhalf const &block_out) -> arma::Mat<complex> {
+            return matrix_cplx(bonds, block_in, block_out);
+          },
+          [&](tJ const &block_in, tJ const &block_out) -> arma::Mat<complex> {
+            return matrix_cplx(bonds, block_in, block_out);
+          },
+          [&](Electron const &block_in,
+              Electron const &block_out) -> arma::Mat<complex> {
+            return matrix_cplx(bonds, block_in, block_out);
+          },
+          [&](auto const &block_in, auto const &block_out) {
+            Log.err("Error in matrix_real: Invalid blocks (combination)!");
+            return arma::cx_mat();
+          }},
+      block_in, block_out);
 }
 arma::Mat<complex> matrix_cplx(Bond const &bond, Block const &block_in,
                                Block const &block_out) {
