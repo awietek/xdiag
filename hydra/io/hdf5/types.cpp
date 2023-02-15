f92c948376b241a64d3edb389ef8f0aca197e419
Alexander Wietek <awietek@pks.mpg.de>
2023-02-15 11:09:37 +0100

    Fixed a few hdf5 things for OSX

--- hydra/io/hdf5/types.cpp
+++ hydra/io/hdf5/types.cpp
@@ -12,14 +12,16 @@ using complex = std::complex<double>;
 template <> hid_t hdf5_datatype<int8_t>() { return H5T_NATIVE_SCHAR; }
 template <> hid_t hdf5_datatype<int16_t>() { return H5T_NATIVE_SHORT; }
 template <> hid_t hdf5_datatype<int32_t>() { return H5T_NATIVE_INT; }
-template <> hid_t hdf5_datatype<int64_t>() { return H5T_NATIVE_LONG; }
-template <> hid_t hdf5_datatype<arma::sword>() { return H5T_NATIVE_LLONG; }
+template <> hid_t hdf5_datatype<long>() { return H5T_NATIVE_LONG; }
+template <> hid_t hdf5_datatype<long long>() { return H5T_NATIVE_LLONG; }
 
 template <> hid_t hdf5_datatype<uint8_t>() { return H5T_NATIVE_UCHAR; }
 template <> hid_t hdf5_datatype<uint16_t>() { return H5T_NATIVE_USHORT; }
 template <> hid_t hdf5_datatype<uint32_t>() { return H5T_NATIVE_UINT; }
-template <> hid_t hdf5_datatype<uint64_t>() { return H5T_NATIVE_ULONG; }
-template <> hid_t hdf5_datatype<arma::uword>() { return H5T_NATIVE_ULLONG; }
+template <> hid_t hdf5_datatype<unsigned long>() { return H5T_NATIVE_ULONG; }
+template <> hid_t hdf5_datatype<unsigned long long>() {
+  return H5T_NATIVE_ULLONG;
+}
 
 template <> hid_t hdf5_datatype<double>() { return H5T_NATIVE_DOUBLE; }
 template <> hid_t hdf5_datatype<complex>() {
@@ -35,14 +37,14 @@ template <> bool hdf5_datatype_mutable<complex>() { return true; }
 template bool hdf5_datatype_mutable<int8_t>();
 template bool hdf5_datatype_mutable<int16_t>();
 template bool hdf5_datatype_mutable<int32_t>();
-template bool hdf5_datatype_mutable<int64_t>();
-template bool hdf5_datatype_mutable<arma::sword>();
+template bool hdf5_datatype_mutable<long>();
+template bool hdf5_datatype_mutable<long long>();
 
 template bool hdf5_datatype_mutable<uint8_t>();
 template bool hdf5_datatype_mutable<uint16_t>();
 template bool hdf5_datatype_mutable<uint32_t>();
-template bool hdf5_datatype_mutable<uint64_t>();
-template bool hdf5_datatype_mutable<arma::uword>();
+template bool hdf5_datatype_mutable<unsigned long>();
+template bool hdf5_datatype_mutable<unsigned long long>();
 
 template bool hdf5_datatype_mutable<double>();
 
