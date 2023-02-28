5d728dcbe36d77d7dfe6c431116c4db96061a43f
Alex Wietek <awietek@pks.mpg.de>
2023-02-28 17:20:13 +0100

    changed standard depth of time evolution algorithm

--- hydra/algorithms/time_evolution/zahexpv.h
+++ hydra/algorithms/time_evolution/zahexpv.h
@@ -12,7 +12,7 @@ namespace hydra {
 std::tuple<double, double>
 zahexpv(double time,
         std::function<arma::cx_vec(arma::cx_vec const &)> const &apply_A,
-        arma::cx_vec &v, double tol = 1e-12, int m = 5, double anorm = 0.,
+        arma::cx_vec &v, double tol = 1e-12, int m = 30, double anorm = 0.,
         int nnorm = 2);
 
 }
