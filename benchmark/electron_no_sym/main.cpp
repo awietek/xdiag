6f18e27736e17c9b0f1c9f226886c29ec6c3597e
Alex Wietek <awietek@pks.mpg.de>
2023-03-08 09:52:17 +0100

    Added benchmarks

---
+++ benchmark/electron_no_sym/main.cpp
@@ -0,0 +1,31 @@
+#include <hydra/all.h>
+
+int main() {
+  using namespace hydra;
+  using namespace arma;
+  using fmt::format;
+
+  Log.set_verbosity(2);
+  
+  int n_sites = 14;
+  int nup = 6;
+  int ndn = 6;
+      
+  BondList bonds;
+  for (int s1 = 0; s1 < n_sites; ++s1) {
+    int s2 = (s1+1) % n_sites;
+    bonds << Bond("HOP", "T", {s1, s2});
+    bonds << Bond("ISING", "JZ", {s1, s2});
+    bonds << Bond("EXCHANGE", "JEX", {s1, s2});
+  }
+  bonds["T"] = 1.0;
+  bonds["JZ"] = 0.0;
+  bonds["JEX"] = 0.0;
+
+  auto block = Electron(n_sites, nup, ndn);
+  HydraPrint(block);
+  auto e0 = eig0(bonds, block);
+  HydraPrint(e0);
+
+  return EXIT_SUCCESS;
+}
