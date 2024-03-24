fcc260c3fde04cf5e574c64bbee36fd15c00a592
Alexander Wietek <awietek@pks.mpg.de>
2024-03-24 22:29:59 +0100

    Added new hash_div3, messed up CMake

--- examples/time_evolution/tj_distributed_time_evolve/main.cpp
+++ examples/time_evolution/tj_distributed_time_evolve/main.cpp
@@ -1,11 +1,12 @@
 #include <hydra/all.h>
 
-void measure_density(int n_sites, hydra::State const &v) {
-  using namespace hydra;
+using namespace hydra;
+
+void measure_density(int n_sites, State const &v) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   for (int i = 0; i < n_sites; ++i) {
-    auto sz = inner(Bond("NUMBER", i), v);
+    complex sz = innerC(Bond("NUMBER", i), v);
     if (rank == 0) {
       printf("%.6f ", std::real(sz));
     }
@@ -15,36 +16,40 @@ void measure_density(int n_sites, hydra::State const &v) {
   }
 }
 
-int main() {
-  using namespace hydra;
+int main(int argc, char **argv) try {
+  MPI_Init(&argc, &argv);
 
-  int L = 4;
+  int L = 6;
   int W = 4;
   double t = 1.0;
-  double J = 0.4;
-  double mu_0 = 2;
+  double J = 0.1;
+  double mu_0 = 10;
 
-  int n_sites = L * L;
+  int n_sites = L * W;
   double precision = 1e-12;
 
   // Create square lattice t-J model
   BondList bonds;
-  for (int x = 0; x < L; ++x) {
+  for (int x = 0; x < L-1; ++x) {
     for (int y = 0; y < W; ++y) {
       int nx = (x + 1) % L;
       int ny = (y + 1) % W;
 
-      int site = y * L + x;
-      int right = y * L + nx;
-      int top = ny * L + x;
+      int site = x * W + y;
+      int right = nx * W + y;
+      int top = x * W + ny;
       bonds << Bond("HOP", "T", {site, right});
-      bonds << Bond("TJISING", "J", {site, right});
+      bonds << Bond("EXCHANGE", "J", {site, right});
       bonds << Bond("HOP", "T", {site, top});
-      bonds << Bond("TJISING", "J", {site, top});
+      bonds << Bond("EXCHANGE", "J", {site, top});
 
+
+      
       if (x < L / 2) {
+	Log("x {} y {} site {} t {} r {} +", x, y, site, top, right);
         bonds << Bond("NUMBER", "MUPLUS", site);
       } else {
+	Log("x {} y {} site {} t {} r {} -", x, y, site, top, right);
         bonds << Bond("NUMBER", "MUNEG", site);
       }
     }
@@ -54,8 +59,14 @@ int main() {
   bonds["MUPLUS"] = mu_0;
   bonds["MUNEG"] = mu_0;
 
-  auto block = tJDistributed(n_sites, n_sites / 2, n_sites / 2 - 1);
+  auto block = tJDistributed(n_sites, n_sites / 2 - 1, n_sites / 2 - 1);
+
+  HydraPrint(block);
+
+  Log.set_verbosity(2);
+  tic();
   auto [e0, v] = eig0(bonds, block);
+  toc("gs");
 
   bonds["MUPLUS"] = 0;
   bonds["MUNEG"] = 0;
@@ -65,9 +76,16 @@ int main() {
   // Do the time evolution with a step size tau
   double tau = 0.1;
   for (int i = 0; i < 40; ++i) {
+    tic();
     v = time_evolve(bonds, v, tau, precision);
+    toc("time evolve");
+    tic();
     measure_density(n_sites, v);
+    toc("measure");
   }
 
+  MPI_Finalize();
   return EXIT_SUCCESS;
+} catch (std::exception const &e) {
+  traceback(e);
 }
