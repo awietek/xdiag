a8c77b9cbaaa200933488749c2326b64d5590e3e
Alexander Wietek <awietek@pks.mpg.de>
2025-12-03 16:40:22 +0100

    switched to safe combinations iterator

--- xdiag/combinatorics/bit_patterns.hpp
+++ xdiag/combinatorics/bit_patterns.hpp
@@ -14,18 +14,20 @@ template <typename bit_t> constexpr bit_t get_next_pattern(bit_t v) noexcept {
   // http://graphics.stanford.edu/~seander/bithacks.html
   // #NextBitPermutation
 
-  // Fast version (needs __builtin_ctz(v)) (some problem with 0)
-  bit_t t = v | (v - 1); // t gets v's least significant 0
-  return ((t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1)));
+  // DONT USE FAST VERSIONS
+
+  // // Fast version (needs __builtin_ctz(v)) (some problem with 0)
+  // bit_t t = v | (v - 1); // t gets v's least significant 0
+  // return ((t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1)));
 
   // // Fast version (needs __builtin_ctz(v)) (some problem with 0)
   // int_t t = v | (v - 1); // t gets v's least significant 0
   // return v == 0 ? ~v :((t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) +
   // 1)));
 
-  // // Slow version (should work everywhere)
-  // int_t t = (v | (v - 1)) + 1;
-  // return v == 0 ? ~v : t | ((((t & -t) / (v & -v)) >> 1) - 1);
+  // Slow version (should work everywhere)
+  bit_t t = (v | (v - 1)) + 1;
+  return v == 0 ? ~v : t | ((((t & -t) / (v & -v)) >> 1) - 1);
 }
 
 template <typename bit_t>
